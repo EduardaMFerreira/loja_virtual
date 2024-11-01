fbda4842106166981b3013c17c999bc53aed0aed
Eduarda Ferreira <166639823+EduardaMFerreira@users.noreply.github.com>
2024-11-01 08:29:34 -0300

    atualização cabeçalho

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="pt-br
+">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Fashion Luar</title>
+</head>
+<body>
+    <header class="cabecalho">
+        <section class="cabecalho__conteudo">
+            <img class="cabecalho__conteudo__imagem" src="./assets/images/logo.jpg" alt="Logo da loja" >
+
+            <form action="#" method="GET" class="cabecalho__conteudo_pesquisa">
+                <button class="cabecalho__conteudo__pesquisa__lupa"><img src="./assets/images/icons/lupa.png" alt="Lupa de pesquisa"></button>
+                <input class="cabecalho__conteudo__pesquisa__barra" type="search" name="pesquisa" placeholder="Digite o item desejado">
+            </form>
+
+            <div class="cabecalho__conteudo__acesso">
+                <a href="#"><img class="cabecalho__conteudo__acesso__imagem" src="./assets/images/icons/sacola_compras.png" alt="Sacola de compras"></a>
+
+                <a href="#"><img class="cabecalho__conteudo__acesso__imagem" src="./assets/images/icons/perfil.png" alt="perfil do usuário"></a>
+            </div>
+        </section>
+
+        <nav class="cabecalho__conteudo__navegacao">
+            <a class="cabecalho__conteudo__navegacao__link" href="#">Home</a>
+            <a class="cabecalho__conteudo__navegacao__link" href="#">Novidades</a>
+            <a class="cabecalho__conteudo__navegacao__link" href="#">Promoções</a>
+            <a class="cabecalho__conteudo__navegacao__link" href="#">Vestuário</a>
+        </nav>
+    </header>
+    <main class="conteudo">
+        <section class="conteudo__atrativos">
+            <div>
+
+            </div>        </section>
+        <section class="conteudo__secao" style="background-color: white;">
+            <h2>NOVIDADES</h2>
+            <div class="conteudo__secao__classificacao">
+                
+                <div class="conteudo__secao__classificacao__numero" style="background-color: #D9D9D9;">
+                
+                </div>
+                <div class="conteudo__secao__classificacao__numero" style="background-color: #D9D9D9;">
+                
+                </div>  
+                <div class="conteudo__secao__classificacao__numero" style="background-color: #D9D9D9;">
+                
+                </div>
+            </div>
+        </section>
+        <section class="conteudo__secao">
+            <h2>MAIS VENDIDOS</h2>
+
+            <div class="conteudo__secao__classificacao">
+                <div class="conteudo__secao__classificacao__numero">
+                </div>
+                <div class="conteudo__secao__classificacao__numero">
+                </div>
+                <div class="conteudo__secao__classificacao__numero">
+                
+                </div>
+            </div>
+        </section>
+        <section class="conteudo__frase">
+
+        </section>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
