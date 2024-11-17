7f7cec82080b14942411cd8828c0440a4a17d732
Eduarda Ferreira <166639823+EduardaMFerreira@users.noreply.github.com>
2024-11-17 15:25:52 -0300

    adicionando seção novidades

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
-<html lang="pt-br
-">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,6 +7,7 @@
     <title>Fashion Luar</title>
 </head>
 <body>
+
     <header class="cabecalho">
         <section class="cabecalho__conteudo">
             <img class="cabecalho__conteudo__imagem" src="./assets/images/logo.jpg" alt="Logo da loja">
@@ -31,12 +31,45 @@
             <a class="cabecalho__conteudo__navegacao__link" href="#conteudo__vestuario">Vestuário</a>
         </nav>
     </header>
+
     <main class="conteudo">
         <section>
             <img class="conteudo__imagem" src="./assets/images/coleção.jpg" alt="Representação de atrativos">
         </section> 
         <section id="conteudo__novidades">
-            
+            <h2>Novidades</h2>
+            <section class="conteudo__novidades__bleizers">
+    
+                <div class="conteudo__novidades__variedade">
+                    <img src="./assets/images/novidades/bleizer_cinza.png" alt="Bleizer cinza">
+                    <h3>Bleizer Cinza</h3>
+                    <p class="preco">R$ 99,99</p>
+                    <p class="parcelado">ou 5X de R$ 19,99 sem juros</p>
+                    <a href="#">Comprar agora</a>
+                </div>
+                <div class="conteudo__novidades__variedade">
+                    <img src="./assets/images/novidades/bleizer_verde.png" alt="Bleizer verde">
+                    <h3>Bleizer Verde</h3>
+                    <p class="preco">R$ 99,99</p>
+                    <p class="parcelado">ou 5X de R$ 19,99 sem juros</p>
+                    <a href="#">Comprar agora</a>
+                </div>
+                <div class="conteudo__novidades__variedade">
+                    <img src="./assets/images/novidades/bleizer_vinho.png" alt="Bleizer vinho">
+                    <h3>Bleizer Vinho</h3>
+                    <p class="preco">R$ 99,99</p>
+                    <p class="parcelado">ou 5X de R$ 19,99 sem juros</p>
+                    <a href="#">Comprar agora</a>
+                </div>
+                <div class="conteudo__novidades__variedade">
+                    <img src="./assets/images/novidades/bleizer_vermelho.png" alt="Bleizer vermelho">
+                    <h3>Bleizer Vermelho</h3>
+                    <p class="preco">R$ 99,99</p>
+                    <p class="parcelado">ou 5X de R$ 19,99 sem juros</p>
+                    <a href="#">Comprar agora</a>
+                </div>
+            </section>
+
         </section >
         <section id="conteudo__vendidos">
             
@@ -48,6 +81,7 @@
 
         </section>
     </main>
+
     <footer class="rodape">
 
     </footer>
